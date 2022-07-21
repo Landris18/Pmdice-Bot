b20a51c07a9b123175810ddf3fdbefd3842ef9e7
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2022-07-21 21:42:11 +0300

    [IMP] Refactorisation de login et ajout de function de verification de chargement complète de la page

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from os import environ as env
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 driver = webdriver.Firefox()
 
 driver.get("https://pmdice.com")
 
-driver.find_element(by=By.XPATH, value="//u[contains(text(),'Login')]").click()
 
-username_input = driver.find_element(by=By.ID, value="loginname")
-password_input = driver.find_element(by=By.ID, value="loginpw")
-button_login = driver.find_element(by=By.ID, value="btnlogin")
-
-username_input.send_keys("")
-password_input.send_keys("")
-button_login.click()
+def page_has_loaded():
+    return driver.execute_script('return document.readyState;') == 'complete'
 
 
+def login():
+    driver.find_element(by=By.XPATH, value="//u[contains(text(),'Login')]").click()
+    driver.find_element(by=By.ID, value="loginname").send_keys(env.get("USERNAME"))
+    driver.find_element(by=By.ID, value="loginpw").send_keys(env.get("PASSWORD"))
+    driver.find_element(by=By.ID, value="btnlogin").click()
+
+
+if __name__ == "__main__":
+    while not page_has_loaded():
+        sleep(0.5)
+    login()
